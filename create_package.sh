b0925c2cefc4eee80f9236631fbb061d40307249
Christoph Petrausch <christoph.petrausch@inovex.de>
2019-07-15 11:14:54 +0200

    Use onliner to find directory of the script.

--- create_package.sh
+++ create_package.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 set -eu
 
-export DEST="./packages"
+# Find the script location. Inspired by: https://stackoverflow.com/questions/59895/get-the-source-directory-of-a-bash-script-from-within-the-script-itself
+scriptLocation="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+export DEST="${scriptLocation}/packages"
 
 rm -rf "${DEST}/*"
 mkdir -p "${DEST}"
