901702409806049e9a83a538fdbebdd7691f6f44
Johannes M. Scheuermann <joh.scheuer@gmail.com>
2020-06-22 06:13:40 +0200

    Formate code with black

--- mail/send_mails.py
+++ mail/send_mails.py
@@ -16,7 +16,8 @@ import yaml
 
 
 # If modifying these scopes, delete the file token.pickle.
-SCOPES = ['https://www.googleapis.com/auth/gmail.send']
+SCOPES = ["https://www.googleapis.com/auth/gmail.send"]
+
 
 def create_message_with_attachment(sender, receiver, subject, message_text, file):
     """Create a message for an email.
@@ -32,9 +33,9 @@ def create_message_with_attachment(sender, receiver, subject, message_text, file
       An object containing a base64url encoded email object.
     """
     message = MIMEMultipart()
-    message['to'] = receiver
-    message['from'] = sender
-    message['subject'] = subject
+    message["to"] = receiver
+    message["from"] = sender
+    message["subject"] = subject
 
     msg = MIMEText(message_text)
     message.attach(msg)
@@ -45,13 +46,13 @@ def create_message_with_attachment(sender, receiver, subject, message_text, file
 
     main_type, sub_type = content_type.split("/", 1)
     msg = MIMEBase(main_type, sub_type)
-    with open(file, 'rb') as content:
+    with open(file, "rb") as content:
         msg.set_payload(content.read())
 
     # Fixes malformed content: https://www.w3.org/Protocols/rfc1341/5_Content-Transfer-Encoding.html
     encoders.encode_base64(msg)
     filename = os.path.basename(file)
-    msg.add_header('Content-Disposition', 'attachment', filename=filename)
+    msg.add_header("Content-Disposition", "attachment", filename=filename)
     message.attach(msg)
 
     # the message should converted from string to bytes.
@@ -59,7 +60,8 @@ def create_message_with_attachment(sender, receiver, subject, message_text, file
     # encode in base64 (printable letters coding)
     message_as_base64 = base64.urlsafe_b64encode(message_as_bytes)
     # need to JSON serializable (no idea what does it means)
-    return {'raw': message_as_base64.decode()}
+    return {"raw": message_as_base64.decode()}
+
 
 def send_message(service, user_id, message):
     """Send an email message.
@@ -74,12 +76,14 @@ def send_message(service, user_id, message):
       Sent Message.
     """
     try:
-        message = (service.users().messages().send(userId=user_id, body=message)
-                   .execute())
-        print('Message Id: {}'.format(message['id']))
+        message = (
+            service.users().messages().send(userId=user_id, body=message).execute()
+        )
+        print("Message Id: {}".format(message["id"]))
         return message
     except errors.HttpError as error:
         print("An error occurred: {}".format(error))
+
 
 def main():
     """Shows basic usage of the Gmail API.
@@ -89,22 +93,21 @@ def main():
     # The file token.pickle stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
     # time.
-    if os.path.exists('token.pickle'):
-        with open('token.pickle', 'rb') as token:
+    if os.path.exists("token.pickle"):
+        with open("token.pickle", "rb") as token:
             creds = pickle.load(token)
     # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
+            flow = InstalledAppFlow.from_client_secrets_file("credentials.json", SCOPES)
             creds = flow.run_local_server(port=0)
         # Save the credentials for the next run
-        with open('token.pickle', 'wb') as token:
+        with open("token.pickle", "wb") as token:
             pickle.dump(creds, token)
 
-    service = build('gmail', 'v1', credentials=creds)
+    service = build("gmail", "v1", credentials=creds)
 
     # Configure mail boilerplate
     mail_info = yaml.safe_load(open("mail_info.yaml"))
@@ -124,9 +127,10 @@ def main():
             attendee["Mail"],
             subject,
             text_template.format(**attendee),
-            "../packages/{}.zip".format(attendee["Short"]))
+            "../packages/{}.zip".format(attendee["Short"]),
+        )
         send_message(service, "me", msg)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
