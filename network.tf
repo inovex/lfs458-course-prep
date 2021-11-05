4aad80bf68cfcf87c08872218a67b6b8e6128a67
Christian Rohmann <christian.rohmann@inovex.de>
2021-11-05 14:52:51 +0100

    Change port_range_min and port_range_max to 0 as "any" to avoid constant changes

--- network.tf
+++ network.tf
@@ -46,8 +46,8 @@ resource "openstack_networking_secgroup_rule_v2" "sec-all" {
   direction         = "ingress"
   ethertype         = "IPv4"
   protocol          = "tcp"
-  port_range_min    = 1
-  port_range_max    = 65535
+  port_range_min    = 0
+  port_range_max    = 0
   remote_ip_prefix  = "0.0.0.0/0"
   security_group_id = openstack_networking_secgroup_v2.sec.id
 }
